7c8778709e053c13a96de3f9a3ec38d489cf49d2
אושר כהן <osher@h-MacBook-Pro-sl-wsr.local>
2023-11-09 17:38:50 +0200

    trying to deploy

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ passport.deserializeUser(function(id,done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://osher-site-2499c2af4256.herokuapp.com/auth/google/secrets"
+    callbackURL: process.env.PORT + "auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
 
@@ -166,7 +166,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.CLIENT_ID_FACEBOOK,
     clientSecret: process.env.CLIENT_SECRET_FACEBOOK,
-    callbackURL: "https://osher-site-2499c2af4256.herokuapp.com/auth/facebook/secrets",
+    callbackURL:  process.env.PORT + "auth/facebook/secrets",
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -185,7 +185,7 @@ passport.use(new FacebookStrategy({
 passport.use(new GitHubStrategy({
     clientID: process.env.CLIENT_ID_GITHUB,
     clientSecret: process.env.CLIENT_SECRET_GITHUB,
-    callbackURL: "https://osher-site-2499c2af4256.herokuapp.com/auth/github/secrets"
+    callbackURL: process.env.PORT + "auth/github/secrets"
   },
   function(accessToken, refreshToken, profile, done) {
 
